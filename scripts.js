6e3c55faf64de084a231d431a5e579b6fb6b0867
Bulat <57670154+MadPuffer@users.noreply.github.com>
2022-02-26 04:26:20 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -10,6 +10,23 @@ $(document).ready(function () {
     }
 });
 
+function getCaretPos(obj)
+{
+
+    if(obj.selectionStart) return obj.selectionStart;
+    else if (document.selection)
+    {
+        var sel = document.selection.createRange();
+        var clone = sel.duplicate();
+        sel.collapse(true);
+        clone.moveToElementText(obj);
+        clone.setEndPoint('EndToEnd', sel);
+        return clone.text.length;
+    }
+
+    return 0;
+}
+
 $("#input-box").keyup(function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
@@ -25,7 +42,7 @@ function enterCommand(inputArea) {
 
     let pastCommands = document.getElementById("editableBox");
 
-    let commandLine = document.createElement("span");
+    let commandLine = document.createElement("span")
     commandLine.className = "command-line";
     commandLine.innerText = "C:\\Users\\Billy>" + val + "\n";
     pastCommands.insertAdjacentElement("beforeend", commandLine);
@@ -33,37 +50,22 @@ function enterCommand(inputArea) {
     // handler
 
     let commands = {
-        "ver" : ver,
-        "help" : help
+        "ver" : new Command("ver", "Displays the CLE version.", ver),
+        "help" : new Command("help", "Provides Help information for CLE.", help),
+        "cls" : new Command("cls", "Clears the screen.", cls),
+        "cle" : new Command("cle", "Starts a new instance of the Web command interpreter.", ver),
     };
 
 
     if (commands.hasOwnProperty(val)) {
         if (val === "help")
         {
-            help(commands);
-            return 0;
+            help(commands)
+            return 0
         }
-        commands[val];
-    }
-
-}
-
-function ver() {
-    let pastCommands = document.getElementById("editableBox");
-    let commandLine = document.createElement("span");
-    commandLine.className = "command-line";
-    commandLine.innerText = "C:\\Users\\Billy>" + "CommandLine Emulator [Version 0.0.6.9]" + "\n";
-    pastCommands.insertAdjacentElement("beforeend", commandLine);
-}
-
-function help(commands) {
-    let pastCommands = document.getElementById("editableBox");
-    for (const key in commands) {
-        let commandLine = document.createElement("span");
-        commandLine.className = "command-line";
-        commandLine.innerText = key.toUpperCase() + "\n";
-        pastCommands.insertAdjacentElement("beforeend", commandLine);
+        commands[val].execute();
+    } else {
+        invalidCommand(val)
     }
 
 }
@@ -76,10 +78,45 @@ class Command {
     }
 
     getInfo() {
-        // Return info about this command
+        return this.desc;
     }
 
     execute() {
-        func();
+        this.func();
     }
 }
+
+function invalidCommand(val) {
+    let pastCommands = document.getElementById("editableBox");
+    let commandLine = document.createElement("span")
+    commandLine.className = "command-line";
+    commandLine.innerText = "'" + val + "'" + " is not recognized as an internal command";
+    pastCommands.insertAdjacentElement("beforeend", commandLine);
+}
+
+function ver() {
+    let pastCommands = document.getElementById("editableBox");
+    let commandLine = document.createElement("span")
+    commandLine.className = "command-line";
+    commandLine.innerText = "CommandLine Emulator [Version 0.0.6.9]";
+    pastCommands.insertAdjacentElement("beforeend", commandLine);
+}
+
+function help(commands) {
+    let pastCommands = document.getElementById("editableBox");
+    for (const key in commands) {
+        let commandLine = document.createElement("span")
+        commandLine.className = "command-line";
+        commandLine.innerText = key.toUpperCase() + '\u00A0'.repeat(20 - key.length) + commands[key].getInfo();
+        pastCommands.insertAdjacentElement("beforeend", commandLine);
+    }
+
+}
+
+function cls() {
+    let pastCommands = document.getElementById("editableBox");
+    while(pastCommands.firstChild){
+        pastCommands.removeChild(pastCommands.firstChild);
+    }
+}
+
