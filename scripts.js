c0f38d288249bc9f4a37fb4ff252644a3a477de1
Bulat <57670154+MadPuffer@users.noreply.github.com>
2022-02-27 15:06:42 +0300

    Add files via upload
    Raw version of fileSystem

--- scripts.js
+++ scripts.js
@@ -1,6 +1,32 @@
+class FileSystemObject {
+    constructor(name, type, parent) {
+        this.name = name;
+        this.type = type;
+        this.parent = parent;
+        this.route = parent.getRoute + "\\" + this.name;
+
+    }
+
+    getRoute() {
+        return this.route;
+    }
+
+}
+
+let currentDir = "C:\\"
+let routes = [
+    "c:\\"
+]
+let files = [
+    new FileSystemObject("c:", "directory", new FileSystemObject("", "", ""))
+]
+
 $(document).ready(function () {
+    let currDirSpan = document.getElementById("currentDir");
+    currDirSpan.innerText = currentDir + ">"
 
     let input = document.getElementById("input-box");
+    input.focus();
 
     input.addEventListener('input', resizeInput);
     resizeInput.call(input);
@@ -10,22 +36,22 @@ $(document).ready(function () {
     }
 });
 
-function getCaretPos(obj)
-{
-
-    if(obj.selectionStart) return obj.selectionStart;
-    else if (document.selection)
-    {
-        var sel = document.selection.createRange();
-        var clone = sel.duplicate();
-        sel.collapse(true);
-        clone.moveToElementText(obj);
-        clone.setEndPoint('EndToEnd', sel);
-        return clone.text.length;
-    }
-
-    return 0;
-}
+// function getCaretPos(obj)
+// {
+//
+//     if(obj.selectionStart) return obj.selectionStart;
+//     else if (document.selection)
+//     {
+//         var sel = document.selection.createRange();
+//         var clone = sel.duplicate();
+//         sel.collapse(true);
+//         clone.moveToElementText(obj);
+//         clone.setEndPoint('EndToEnd', sel);
+//         return clone.text.length;
+//     }
+//
+//     return 0;
+// }
 
 $("#input-box").keyup(function (event) {
     if (event.keyCode === 13) {
@@ -44,7 +70,7 @@ function enterCommand(inputArea) {
 
     let commandLine = document.createElement("span")
     commandLine.className = "command-line";
-    commandLine.innerText = "C:\\Users\\Billy>" + val + "\n";
+    commandLine.innerText = currentDir + ">" + val + "\n";
     pastCommands.insertAdjacentElement("beforeend", commandLine);
 
     // handler
@@ -54,18 +80,20 @@ function enterCommand(inputArea) {
         "help" : new Command("help", "Provides Help information for CLE.", help),
         "cls" : new Command("cls", "Clears the screen.", cls),
         "cle" : new Command("cle", "Starts a new instance of the Web command interpreter.", ver),
+        "cd" : new Command("cd", "Displays the name of or changes the current directory.", cd),
+        "md" : new Command("md", "Creates a directory.", md)
     };
 
 
-    if (commands.hasOwnProperty(val)) {
-        if (val === "help")
+    if (commands.hasOwnProperty(val.toLowerCase().split(" ")[0])) {
+        if (val.toLowerCase() === "help")
         {
             help(commands)
             return 0
         }
-        commands[val].execute();
+        commands[val.toLowerCase().split(" ")[0]].execute(val);
     } else {
-        invalidCommand(val)
+        invalidCommand(val.toLowerCase())
     }
 
 }
@@ -81,9 +109,79 @@ class Command {
         return this.desc;
     }
 
-    execute() {
-        this.func();
+    execute(val) {
+        this.func(val);
     }
+}
+
+function cd(val) {
+    let pastCommands = document.getElementById("editableBox");
+    let commandLine = document.createElement("span")
+    commandLine.className = "command-line";
+
+    let route = val.split(" ")[1]
+
+    if (val.split(" ").length === 1) {
+        commandLine.innerText = currentDir;
+        pastCommands.insertAdjacentElement("beforeend", commandLine);
+    } else if (route.split("\\")[0].toLowerCase() === "c:") {
+
+        if (routes.find(r => r === route) === route)
+        {
+            currentDir = val.replace("c:", "C:").split(" ")[1]
+        }
+
+    }
+
+    document.getElementById("currentDir").innerText = currentDir + ">"
+
+    // commandLine.innerText = "'" + val + "'" + " is not recognized as an internal command";
+    // pastCommands.insertAdjacentElement("beforeend", commandLine);
+}
+
+function md(val) {
+    if (val.split(" ").length === 1) {
+        let pastCommands = document.getElementById("editableBox");
+        let commandLine = document.createElement("span")
+        commandLine.className = "command-line";
+        commandLine.innerText = "The syntax of the command is incorrect."
+        pastCommands.insertAdjacentElement("beforeend", commandLine);
+        return 0
+    }
+
+    let route = val.split(" ")[1]
+
+    if (route.split("\\")[0].toLowerCase() === "c:") {
+        routes.push(route);
+
+        let splittedRoute = route.split("\\")
+        let name = splittedRoute[splittedRoute.length - 1].split(".")[0]
+        let type = splittedRoute[splittedRoute.length - 1].split(".")[1]
+        if (type === undefined) {
+            type = "directory"
+        }
+        let parent = files.find(p => p.name === splittedRoute[splittedRoute.length - 2])
+        if (parent === undefined) {
+            createParent(splittedRoute[splittedRoute.length - 2], splittedRoute[splittedRoute.length - 3], route, 3)
+            parent = files.find(p => p.name === splittedRoute[splittedRoute.length - 1])
+        }
+
+        files.push(new FileSystemObject(name, type, parent))
+
+        console.log(routes)
+    }
+}
+
+function createParent(name, parent, route, scope)
+{
+
+    let splittedRoute = route.split()
+
+    let pr = files.find(p => p.name === parent)
+    if (pr === undefined) {
+        createParent(splittedRoute[splittedRoute.length - scope], splittedRoute[splittedRoute.length - scope - 1], route, scope - 1)
+    }
+    files.push(name, "directory", pr)
 }
 
 function invalidCommand(val) {
@@ -94,7 +192,7 @@ function invalidCommand(val) {
     pastCommands.insertAdjacentElement("beforeend", commandLine);
 }
 
-function ver() {
+function ver(val) {
     let pastCommands = document.getElementById("editableBox");
     let commandLine = document.createElement("span")
     commandLine.className = "command-line";
@@ -113,7 +211,7 @@ function help(commands) {
 
 }
 
-function cls() {
+function cls(val) {
     let pastCommands = document.getElementById("editableBox");
     while(pastCommands.firstChild){
         pastCommands.removeChild(pastCommands.firstChild);
